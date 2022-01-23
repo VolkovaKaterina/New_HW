03d7dcff93c7bb6d4cdacd1c47f741c1257e569c
Volkova Katerina <volkovakate87@gmail.com>
2022-01-23 19:09:14 +0200

    closure-2

--- closure/script.js
+++ closure/script.js
@@ -1,25 +1,24 @@
-// Реализовать функции sum через замыкание
-function sum(x) {// ваш код}
+// 1
+function sum(x) {
     return function (y) {
         return x + y
     }
 }
-    const sum1 = sum(1);
-    console.log(sum1(2)); // результат выполнения 3
-    console.log(sum(1)(2)); // результат выполнения 3
 
+const sum1 = sum(1);
+console.log(sum1(2));
+console.log(sum(1)(2));
 
-// Описать комментарием в чем ошибка, почему и исправить код.
-    for (var i = 0; i < 10; i++) {
-        setTimeout(function() {
-            console.log(i);
-        }, 0);
-    }
+// 2
+for (var i = 0; i < 10; i++) {
+    setTimeout(function () {
+        console.log(i);
+    }, 0);
+}
 //Ошибка в том что в консоль выведется десять раз 10
 // тк инициализация переменной через var, и ее значение изменяется в глобальной области видимисти и
 // не сохраняется в block scope, исправить можно повеняв var на let или обернуть в функцию
 
-// debugger
 for (let i = 1; i <= 10; i++) {
     setTimeout(function () {
         console.log(i);
@@ -31,4 +30,4 @@ for (var i = 1; i <= 10; i++) {
         console.log(j);
     }, 0))(i);
 }
-// В итоге нужно вывести от 1 до 10.
+
